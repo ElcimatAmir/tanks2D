76cd1fe03d23c0fd13807f7c220c843718dcd032
root <root@localhost.localdomain>
2019-02-06 01:16:43 +0300

    add a save user data to the local storge  added a css for the page style , nothing with graphics yet

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
-
 //FIRST TIME BASIC SETUP
 	//its a var to know if game allready playing or no
 	var gameRunning = false ;
 	
 	//set the value of input to the user name
 	document.getElementById("playerName").value = "";
+
+	//seting the players records at the begining
+	window.onload = setRecords();
+
+	function setRecords(){
+		const memory = window.localStorage ;
+		var info = {};
+		for (var i = 0; i < memory.length; i++){
+			info.name  = memory.key(i);
+			info.score = memory.getItem(info.name); 
+
+			//also theis method before get the values to show showed sorted them			
+			
+			drawHtml(info) ;
+		}
+	}
+
 
 //when button play get clicked
 document.getElementById("playButton").onclick = function(){
@@ -21,19 +37,36 @@ document.getElementById("playButton").onclick = function(){
 }
 
 function startGame(playerName){
+	var canvas  = document.getElementById("canvas");
+
 	var playerInfo = {
-		Name: playerName ,
+		name: playerName ,
 		score: 0
 	}
-
+	//for this time its set a randoum number for score
+	playerInfo.score =  Math.floor(Math.random() * (100 - 1) + 1) ;
 	console.log(playerInfo);
 
+	// save the player data to the local storage
 	savePlayer(playerInfo);
 }
 
 function savePlayer(playerInfo){
-	var recordsPanel = document.getElementById("playersRecords");
-	/*
-		here i should add a table and save a data in memory
-	*/
+	const memory = window.localStorage ;
+	memory.setItem(playerInfo.name , playerInfo.score)
+	
+	drawHtml(playerInfo);	
+}
+
+
+
+function drawHtml(playerInfo){
+	var html = "" ;
+	var recordsPanel = document.getElementById("playersResult");
+
+	html += "<div class='playersInfo'>"
+				+"<h4>"+playerInfo.name+"</h4>"
+				+"<h4>"+playerInfo.score+"</h4>"
+			+"</div>" ;
+	$(recordsPanel).append(html);		
 }
